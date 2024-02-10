721d1ea5e67ab85e38c97db9b8a768f4f93814bc
Yuxiang Dong <yuxiang.dong@stonybrook.edu>
2024-02-09 22:51:29 -0500

    Fix tailwind setup

--- src/Components/TOBECHANGED.jsx
+++ src/Components/TOBECHANGED.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const TextComponent = () => {
     return (
         <div>
-            <h1>Hello, World!</h1>
-            <p>This is a text component.</p>
+            <h1 className="font-bold text-xl">Hello, World!</h1>
+            <p className="underline underline-offset-1">This is a text component.</p>
+            <p className="underline underline-offset-1">This is a text component.</p>
         </div>
     );
 };
